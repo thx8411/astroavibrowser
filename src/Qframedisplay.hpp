b9bd7adfefad05c7e38a4c47c09d8413ef2b003e
Thx8411 <Thx8411@f887cd58-5af9-bfb7-8131-be430dde9e3c>
2009-11-21 16:57:20 +0000

    License stuff added

--- src/Qframedisplay.hpp
+++ src/Qframedisplay.hpp
@@ -1,4 +1,22 @@
-/* part of AstroAviBrowser, (c) Blaise-Florentin Collin 2009 */
+/*
+ * copyright (c) 2009 Blaise-Florentin Collin
+ *
+ * This file is part of AstroAviBrowser.
+ *
+ * AstroAviBrowser is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License v3
+ * as published by the Free Software Foundation
+ *
+ * AstroAviBrowser is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * v3 along with AstroAviBrowser; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301 USA
+ */
 
 #ifndef _QFRAMEDISPLAY_HPP_
 #define _QFRAMEDISPLAY_HPP_
