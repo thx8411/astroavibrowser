0d66a3a4750b6bfee3146dedf1eba6dda258338d
Thx8411 <Thx8411@f887cd58-5af9-bfb7-8131-be430dde9e3c>
2010-02-15 16:37:26 +0000

    bmp output beginning

---
+++ src/bmpwriter.cpp
@@ -0,0 +1,39 @@
+/*
+ * copyright (c) 2009-2010 Blaise-Florentin Collin
+ *
+ * This file is part of AstroAviBrowser.
+ *
+ * AstroAviBrowser is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License v3
+ * as published by the Free Software Foundation
+ *
+ * AstroAviBrowser is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * v3 along with AstroAviBrowser; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301 USA
+ */
+
+#include <stdlib.h>
+#include <string.h>
+
+#include <iostream>
+
+#include "processing.hpp"
+
+#include "bmpwriter.hpp"
+
+using namespace std;
+
+BmpWriter::BmpWriter(int codec, int plans, const char* name, int width, int height, int frameRate) {
+}
+
+BmpWriter::~BmpWriter() {
+}
+
+void BmpWriter::AddFrame(unsigned char* datas) {
+}
